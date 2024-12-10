b3912d8ed78ba8636f1d0a4bc844716be0b8c480
veloSecreto <nahmedkhan231@gmail.com>
2024-12-10 16:34:58 +0600

    shifted to sublime and some other stuffs

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -13,16 +13,17 @@ void Game::start() {
     AssetManager::loadAll();
 
     GameObject house("house", "default");
-    house.transform.scale = glm::vec3(0.2);
+    house.transform.scale = glm::vec3(0.2f);
     scenes["main"].add(house);
 
     GameObject man("nurseGuy", "default");
-    man.transform.position = glm::vec3(0, 2, 4);
+    man.transform.position = glm::vec3(0, 0.5f, 4);
+    man.transform.scale = glm::vec3(2);
     scenes["main"].add(man);
 
-    scenes["main"].addLight(Light(glm::vec3(6, 3, 0))); // room on the left
-    scenes["main"].addLight(Light(glm::vec3(0, 3, 0), glm::vec3(1, 0.2, 0.2))); // dinning room light, should be red like my one
-    scenes["main"].addLight(Light(glm::vec3(0, 3, 6))); // forward side bed room
+    scenes["main"].addLight(Light(glm::vec3(5, 3, 0))); // room on the left
+    scenes["main"].addLight(Light(glm::vec3(0, 3, 0), glm::vec3(1, 0.2f, 0.2f))); // dinning room light, should be red like my one
+    scenes["main"].addLight(Light(glm::vec3(0, 3, 5))); // forward side bed room
 }
 
 void Game::render() {
